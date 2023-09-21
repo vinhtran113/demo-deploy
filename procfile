c8a39bbdc29568d6bebadd8d24c27e3f17dbc948
vinhtran113 <tvih6693@gmail.com>
2023-09-21 07:33:31 +0700

    hihi

--- procfile
+++ procfile
@@ -1,1 +1,1 @@
-web java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
+web java $JAVA_OPTS -jar target/dependency/webapp-runner-9.0.27.1.jar --port $PORT target/*.war
--- src/main/java/com/example/demodeploy/HelloServlet.java
+++ src/main/java/com/example/demodeploy/HelloServlet.java
@@ -1,9 +1,8 @@
-package com.example.demodeploy;
+package com.example.demodeployhao;
 
 import java.io.*;
-
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
